c89c9c06089c76ec1f2362294483d704e3616118
Ivan Tsai <oscar60310@gmail.com>
2022-05-04 16:32:25 +0800

    fix(core): fix typos in compiler

--- packages/core/src/lib/template-engine/compilers/compiler.ts
+++ packages/core/src/lib/template-engine/compilers/compiler.ts
@@ -1,8 +1,5 @@
-export abstract class Compiler {
-  abstract name: string;
-  abstract compile(template: string): string;
-  abstract render<T extends object>(
-    templateName: string,
-    data: T
-  ): Promise<string>;
+export interface Compiler {
+  name: string;
+  compile(template: string): string;
+  render<T extends object>(templateName: string, data: T): Promise<string>;
 }
