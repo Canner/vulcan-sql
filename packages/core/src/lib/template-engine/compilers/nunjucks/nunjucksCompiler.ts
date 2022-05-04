853606e9b0d0d4a3c5440fa1fa499b2ab6f56f51
Ivan Tsai <oscar60310@gmail.com>
2022-05-04 16:32:25 +0800

    fix(core): fix typos in compiler

--- packages/core/src/lib/template-engine/compilers/nunjucks/nunjucksCompiler.ts
+++ packages/core/src/lib/template-engine/compilers/nunjucks/nunjucksCompiler.ts
@@ -1,23 +1,24 @@
 import { Compiler } from '../compiler';
 import * as nunjucks from 'nunjucks';
 
-const precomileWrapper: nunjucks.PrecompileOptions['wrapper'] = (templates) => {
+const precompileWrapper: nunjucks.PrecompileOptions['wrapper'] = (
+  templates
+) => {
   const template = templates[0].template;
   return `(() => {${template}})()`;
 };
 
-export class NunjucksCompiler extends Compiler {
+export class NunjucksCompiler implements Compiler {
   public name = 'nunjucks';
   private env: nunjucks.Environment;
 
   constructor({ loader }: { loader: nunjucks.ILoader }) {
-    super();
     this.env = new nunjucks.Environment(loader);
   }
 
   public compile(template: string): string {
     return nunjucks.precompileString(template, {
-      wrapper: precomileWrapper,
+      wrapper: precompileWrapper,
       name: 'main',
     });
   }
