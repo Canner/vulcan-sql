44512c3f0d0cc2f481db01f0d2612d13df401549
kokokuo <v6610688@gmail.com>
2022-05-06 12:34:21 +0800

    feature(core): add built-in data type validators and testcase
    - add "DateTypeValidator", "IntegerTypeValidator", "StringTypeValidator", "UUIDTypeValidator" and its test cases.
    - add "loadedValidators" for loading all built-in and user-defined validator dynamically.
    - install joi, moment plugins at package.json in core folder.
    - install faker, ts-sinon, uuid plugins at vulcan package.json.

--- packages/core/src/validators/interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IValidator<T = any> {
-  // validator name
-  name: string;
-  // validate Schema format
-  validateSchema(args: T): boolean;
-  // validate input data
-  validateData(data: string, args: T): boolean;
-}
