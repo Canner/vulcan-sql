d4bba579e84ee655e522b8c97ab851ddcdca4e52
Ivan Tsai <oscar60310@gmail.com>
2022-05-10 12:02:02 +0800

    test(core): add more test cases of core package

---
+++ packages/core/test/template-engine/template-provider/fileTemplateProviderError.spec.ts
@@ -0,0 +1,20 @@
+import { FileTemplateProvider } from '@template-engine/.';
+
+jest.mock('glob', () => {
+  return (
+    _path: string,
+    _option: object,
+    cb: (err: Error, files: string[]) => void
+  ) => {
+    cb(new Error('mock error'), []);
+  };
+});
+
+it('File template provider should throw error with a invalid path', async () => {
+  // Arrange
+  const filePath = '.';
+  const provider = new FileTemplateProvider({ folderPath: filePath });
+  // Act, Assert
+  const iter = provider.getTemplates();
+  await expect(iter.next()).rejects.toThrow('mock error');
+});
