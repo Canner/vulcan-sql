c36c855572a33a755c46044800552ad848a12c4c
kokokuo <v6610688@gmail.com>
2022-06-07 18:34:57 +0800

    feat(serve): add data query builder and test cases.
    - add "DataQueryBuilder" to provide select, join, where, group, having, order, limit, offset by query chain
    - add "JoinOnClause" to provide join-on methods
    - add test cases of "DataQueryBuilder" providing select, join, where, group, having, order, limit, offset clauses.
    - add test cases of "JoinOnClause"  provding on clauses.
    - refactor "baseRoute" for passing template engine and use template engine.
    - fix test cases of "RouteGenerator" and "VulcanApplication".

---
+++ packages/serve/test/data-query/builder/order-by-clause.spec.ts
@@ -0,0 +1,79 @@
+import {
+  DataQueryBuilder,
+  Direction,
+  OrderByClauseOperation,
+} from '@data-query/.';
+import faker from '@faker-js/faker';
+
+describe('Test data query builder > order by clause', () => {
+  it.each([
+    {
+      column: faker.database.column(),
+      direction: Direction.ASC,
+    },
+    {
+      column: faker.database.column(),
+      direction: Direction.DESC,
+    },
+  ])(
+    'Should record successfully when call order by with $column, $direction',
+    async ({ column, direction }) => {
+      // Arrange
+      const expected: Array<OrderByClauseOperation> = [
+        {
+          column,
+          direction,
+        },
+      ];
+
+      // Act
+      const builder = new DataQueryBuilder({
+        statement: 'select * from orders',
+      });
+      builder.orderBy(column, direction);
+
+      // Assert
+      expect(builder.operations.orderBy).toEqual(expected);
+    }
+  );
+
+  it.each([
+    [
+      {
+        column: faker.database.column(),
+        direction: Direction.DESC,
+      },
+      {
+        column: faker.database.column(),
+        direction: Direction.ASC,
+      },
+    ],
+    [
+      {
+        column: faker.database.column(),
+        direction: Direction.ASC,
+      },
+      {
+        column: faker.database.column(),
+        direction: Direction.DESC,
+      },
+    ],
+  ])(
+    'Should record successfully when call orderBy(%p).orderBy(%p)',
+    async (first, second) => {
+      // Arrange
+      const expected: Array<OrderByClauseOperation> = [first, second];
+
+      // Act
+      const builder = new DataQueryBuilder({
+        statement: 'select * from orders',
+      });
+      builder
+        .orderBy(first.column, first.direction)
+        .orderBy(second.column, second.direction);
+
+      // Assert
+      expect(builder.operations.orderBy).toEqual(expected);
+    }
+  );
+});
