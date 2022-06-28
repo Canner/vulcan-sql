9d1003a802d2a31dead236efc5bd37c87994c745
Ivan Tsai <oscar60310@gmail.com>
2022-06-28 15:16:00 +0800

    feat(core): add more constraints
    add MaxValue, MinLength, MaxLength, Regex, Enum constraints

--- packages/core/src/validators/constraints.ts
+++ packages/core/src/validators/constraints.ts
@@ -1,3 +1,5 @@
+import { intersection } from 'lodash';
+
 export abstract class Constraint {
   static Required() {
     return new RequiredConstraint();
@@ -5,6 +7,26 @@ export abstract class Constraint {
 
   static MinValue(minValue: number) {
     return new MinValueConstraint(minValue);
+  }
+
+  static MaxValue(maxValue: number) {
+    return new MaxValueConstraint(maxValue);
+  }
+
+  static MinLength(minLength: number) {
+    return new MinLengthConstraint(minLength);
+  }
+
+  static MaxLength(maxLength: number) {
+    return new MaxLengthConstraint(maxLength);
+  }
+
+  static Regex(regex: string) {
+    return new RegexConstraint(regex);
+  }
+
+  static Enum<T = any>(list: Array<T>) {
+    return new EnumConstraint(list);
   }
 
   abstract compose(constraint: Constraint): Constraint;
@@ -29,6 +51,84 @@ export class MinValueConstraint extends Constraint {
   public compose(constraint: MinValueConstraint): MinValueConstraint {
     return new MinValueConstraint(
       Math.max(this.minValue, constraint.getMinValue())
+    );
+  }
+}
+
+export class MaxValueConstraint extends Constraint {
+  constructor(private maxValue: number) {
+    super();
+  }
+
+  public getMaxValue() {
+    return this.maxValue;
+  }
+
+  public compose(constraint: MaxValueConstraint): MaxValueConstraint {
+    return new MaxValueConstraint(
+      Math.min(this.maxValue, constraint.getMaxValue())
+    );
+  }
+}
+
+export class MinLengthConstraint extends Constraint {
+  constructor(private minLength: number) {
+    super();
+  }
+
+  public getMinLength() {
+    return this.minLength;
+  }
+
+  public compose(constraint: MinLengthConstraint): MinLengthConstraint {
+    return new MinLengthConstraint(
+      Math.max(this.minLength, constraint.getMinLength())
+    );
+  }
+}
+
+export class MaxLengthConstraint extends Constraint {
+  constructor(private maxLength: number) {
+    super();
+  }
+
+  public getMaxLength() {
+    return this.maxLength;
+  }
+
+  public compose(constraint: MaxLengthConstraint): MaxLengthConstraint {
+    return new MaxLengthConstraint(
+      Math.min(this.maxLength, constraint.getMaxLength())
+    );
+  }
+}
+
+export class RegexConstraint extends Constraint {
+  constructor(private regex: string) {
+    super();
+  }
+
+  public getRegex() {
+    return this.regex;
+  }
+
+  public compose(): RegexConstraint {
+    throw new Error(`Can use multiple RegexConstraint at the same time.`);
+  }
+}
+
+export class EnumConstraint<T = string> extends Constraint {
+  constructor(private list: Array<T>) {
+    super();
+  }
+
+  public getList() {
+    return this.list;
+  }
+
+  public compose(constraint: EnumConstraint<any>): EnumConstraint {
+    return new EnumConstraint<any>(
+      intersection(this.getList(), constraint.getList())
     );
   }
 }
--- packages/core/test/validators/constraint.spec.ts
+++ packages/core/test/validators/constraint.spec.ts
@@ -1,5 +1,9 @@
 import {
   Constraint,
+  EnumConstraint,
+  MaxLengthConstraint,
+  MaxValueConstraint,
+  MinLengthConstraint,
   MinValueConstraint,
   RequiredConstraint,
 } from '../../src/validators';
@@ -23,4 +27,56 @@ it('MinValue constraint compose should maximum the value', async () => {
   // Arrange
   expect(composed instanceof MinValueConstraint).toBeTruthy();
   expect((composed as MinValueConstraint).getMinValue()).toBe(2);
+});
+
+it('MaxValue constraint compose should minimize the value', async () => {
+  // Arrange
+  const constraint1: Constraint = Constraint.MaxValue(1);
+  const constraint2 = Constraint.MaxValue(2);
+  // Act
+  const composed = constraint1.compose(constraint2);
+  // Arrange
+  expect(composed instanceof MaxValueConstraint).toBeTruthy();
+  expect((composed as MaxValueConstraint).getMaxValue()).toBe(1);
+});
+
+it('MinLength constraint compose should maximum the value', async () => {
+  // Arrange
+  const constraint1: Constraint = Constraint.MinLength(1);
+  const constraint2 = Constraint.MinLength(2);
+  // Act
+  const composed = constraint1.compose(constraint2);
+  // Arrange
+  expect(composed instanceof MinLengthConstraint).toBeTruthy();
+  expect((composed as MinLengthConstraint).getMinLength()).toBe(2);
+});
+
+it('MaxLength constraint compose should minimize the value', async () => {
+  // Arrange
+  const constraint1: Constraint = Constraint.MaxLength(1);
+  const constraint2 = Constraint.MaxLength(2);
+  // Act
+  const composed = constraint1.compose(constraint2);
+  // Arrange
+  expect(composed instanceof MaxLengthConstraint).toBeTruthy();
+  expect((composed as MaxLengthConstraint).getMaxLength()).toBe(1);
+});
+
+it('RegexConstraint constraint compose should throw error', async () => {
+  // Arrange
+  const constraint1: Constraint = Constraint.Regex('someExp');
+  const constraint2 = Constraint.Regex('someExp');
+  // Act, Arrange
+  expect(() => constraint1.compose(constraint2)).toThrow();
+});
+
+it('Enum constraint compose should return the intersection of them', async () => {
+  // Arrange
+  const constraint1: Constraint = Constraint.Enum([1, 2, 3, 4, 5]);
+  const constraint2 = Constraint.Enum([3, 4, 5, 6, 7]);
+  // Act
+  const composed = constraint1.compose(constraint2);
+  // Arrange
+  expect(composed instanceof EnumConstraint).toBeTruthy();
+  expect((composed as EnumConstraint).getList()).toEqual([3, 4, 5]);
 });
