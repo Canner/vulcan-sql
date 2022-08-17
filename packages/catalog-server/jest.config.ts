2c3657e8340b22c5a861983713a6d712d1f02236
Star <Star2ver.dev@gmail.com>
2022-08-17 12:09:58 +0800

    Add nextjs

---
+++ packages/catalog-server/jest.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'catalog-server',
+  preset: '../../jest.preset.ts',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/catalog-server',
+};
