01d0a9d96759c3c79981a2c210697e4ff354a86a
kokokuo <v6610688@gmail.com>
2022-09-14 12:02:44 +0800

    fix(serve): missing start square bracket when data is empty
    - Fix to add "[" when no data in "_final" method of JsonStringTransformer.
    - add empty data case in json and csv format test case.
    - add response empty data case in integration testing.

--- packages/integration-testing/src/example1/buildAndServe.spec.ts
+++ packages/integration-testing/src/example1/buildAndServe.spec.ts
@@ -57,18 +57,28 @@ afterEach(async () => {
   await server?.close();
 });
 
-it('Example1: Build and serve should work', async () => {
-  const builder = new VulcanBuilder(projectConfig);
-  await builder.build();
-  server = new VulcanServer(projectConfig);
-  const httpServer = (await server.start())['http'];
+it.each([
+  [
+    '436193eb-f686-4105-ad7b-b5945276c14a',
+    [
+      {
+        id: '436193eb-f686-4105-ad7b-b5945276c14a',
+        name: 'ivan',
+      },
+    ],
+  ],
+  ['2dc839e0-0f65-4dba-ac38-4eaf023d0008', []],
+])(
+  'Example1: Build and serve should work',
+  async (userId, expected) => {
+    const builder = new VulcanBuilder(projectConfig);
+    await builder.build();
+    server = new VulcanServer(projectConfig);
+    const httpServer = (await server.start())['http'];
 
-  const agent = supertest(httpServer);
-  const result = await agent.get(
-    '/api/user/436193eb-f686-4105-ad7b-b5945276c14a'
-  );
-  expect(result.body).toContainEqual({
-    id: '436193eb-f686-4105-ad7b-b5945276c14a',
-    name: 'ivan',
-  });
-}, 10000);
+    const agent = supertest(httpServer);
+    const result = await agent.get(`/api/user/${userId}`);
+    expect(JSON.stringify(result.body)).toEqual(JSON.stringify(expected));
+  },
+  10000
+);
