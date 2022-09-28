f97c12e4997b32696783a9257dac66f296987b30
Ivan Tsai <9553914+oscar60310@users.noreply.github.com>
2022-09-28 18:53:17 +0800

    docs(doc): add part of landing page

---
+++ packages/doc/src/components/DiveIn/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styles from './styles.module.css';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+
+const diveInActions = [
+  {
+    icon: require('@site/static/img/discord.svg').default,
+    title: 'Join our developer community',
+    description:
+      'Join the discord group to chat with the developers and connect directly with the VulcanSQL Team',
+    takeActions: (
+      <div className={styles.actionLinks}>
+        <Link to="https://github.com/Canner/vulcan-sql">Go to Github &gt;</Link>
+        <Link to="https://discord.gg/ztDz8DCmG4">Join Discord &gt;</Link>
+      </div>
+    ),
+  },
+  {
+    icon: require('@site/static/img/vulcan.svg').default,
+    title: 'Get started to try VulcanSQL',
+    description: 'Read our documentation and install VulcanSQL now.',
+    takeActions: (
+      <Link
+        className={`button button--primary ${styles.actionButton}`}
+        to="./docs/quickstart"
+      >
+        Get started now
+      </Link>
+    ),
+  },
+];
+
+export default function DiveIn(): JSX.Element {
+  return (
+    <section className={styles.diveIn}>
+      <h1 className={`text--center ${styles.title}`}>Ready to dive in?</h1>
+      <div className="container">
+        <div className={`row ${styles.diveInActionContainer}`}>
+          {diveInActions.map((action) => (
+            <div className={`${clsx('col col--5')} ${styles.diveInAction}`}>
+              {action.icon && (
+                <action.icon className={styles.actionIcon} role="img" />
+              )}
+              <h3 className={styles.actionTitle}>{action.title}</h3>
+              <div className={styles.actionDescription}>
+                {action.description}
+              </div>
+              {action.takeActions}
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
