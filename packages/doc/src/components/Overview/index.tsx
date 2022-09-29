af170656ca9a4a781fb4b1dea7c3650cd4f881e2
william chang <wwwy3y3@gmail.com>
2022-09-29 19:32:08 +0800

    fix(doc): add more width to github btn

--- packages/doc/src/components/Overview/index.tsx
+++ packages/doc/src/components/Overview/index.tsx
@@ -31,7 +31,7 @@ export default function Overview(): JSX.Element {
         src="https://ghbtns.com/github-btn.html?user=canner&repo=vulcan-sql&type=star&count=true&size=large"
         frameBorder="0"
         scrolling="0"
-        width="123"
+        width="150"
         height="30"
         title="GitHub"
       ></iframe>
