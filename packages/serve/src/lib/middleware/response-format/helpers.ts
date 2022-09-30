867dcb219e9701a64693a8c0b3dd19d09ed5d3fd
kokokuo <v6610688@gmail.com>
2022-09-30 10:08:02 +0800

    fix(serve): refactor the checking array type logistic when "formats" is empty for readable.

--- packages/serve/src/lib/middleware/response-format/helpers.ts
+++ packages/serve/src/lib/middleware/response-format/helpers.ts
@@ -29,8 +29,8 @@ export const checkUsableFormat = ({
 
   // if path ending has no format
   if (!pathFormat) {
-    // Use the default when "acceptFormat" us array type, because array type only happened when "supportedFormats" is [] and "Accept" header has multiple
-    if (Array.isArray(acceptFormat)) return defaultFormat;
+    // Use the default format when "supportedFormats" is [].
+    if (supportedFormats.length === 0) return defaultFormat;
     // get default when "Accept" header also not matched or "Accept" header not in request (shows by */*)
     if (!acceptFormat || acceptFormat == '*/*') return defaultFormat;
     // if accept format existed, use "Accept" first matched format by support format order
