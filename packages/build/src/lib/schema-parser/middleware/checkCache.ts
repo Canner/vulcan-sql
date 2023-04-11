043a574217471fd73a0b8adfdb2debceacc5df6b
onlyjackfrost <onlyjackfrost@gmail.com>
2023-04-11 19:34:18 +0800

    add 'every' validation check for refreshTime and refreshExpression

--- packages/build/src/lib/schema-parser/middleware/checkCache.ts
+++ packages/build/src/lib/schema-parser/middleware/checkCache.ts
@@ -1,25 +1,56 @@
 import { ConfigurationError } from '@vulcan-sql/core';
 import { RawAPISchema, SchemaParserMiddleware } from './middleware';
 import { CACHE_METADATA_NAME } from './constants';
+import ms, { StringValue } from 'ms';
 
 export class CheckCache extends SchemaParserMiddleware {
   public async handle(schemas: RawAPISchema, next: () => Promise<void>) {
     // check .yaml file has cache configuration
     const caches = schemas?.cache;
-    if (!caches) return next()
-
-    // throw if cache not used in .sql file 
     const metadata = schemas.metadata;
-    if (!(caches && metadata?.[CACHE_METADATA_NAME]?.['isUsedTag'])){
-      throw new ConfigurationError('your SQL will use the cache feature, not YAML defined.')  
-    } 
+    const isUsedCache = metadata?.[CACHE_METADATA_NAME]?.['isUsedTag'];
+    if (!isUsedCache && !caches) return next();
 
-    // check if refreshTime and refreshExpression is set at the same time
-    caches.forEach(({refreshTime, refreshExpression}): void => {
-      if (refreshTime && refreshExpression){
-        throw new ConfigurationError('can not configure refreshTime and refreshExpression at the same time, please pick one')
-      }
-    });
+    // throw if cache not used in .sql file
+    if (isUsedCache && !caches) {
+      throw new ConfigurationError(
+        "Cache feature was used in SQL file, but didn't find the cache configurations in YAML file."
+      );
+    }
+
+    if (caches) {
+      caches.forEach(({ refreshTime, refreshExpression }): void => {
+        // refreshTime and refreshExpression can not be set at the same time
+        if (refreshTime && refreshExpression) {
+          throw new ConfigurationError(
+            'can not configure refreshTime and refreshExpression at the same time, please pick one'
+          );
+        }
+        // the "every" in refreshTime or refreshExpression should be valid
+        const timeInterval =
+          refreshTime?.['every'] || refreshExpression?.['every'];
+        if (timeInterval) {
+          let interval: number;
+          try {
+            interval = ms(timeInterval as StringValue);
+          } catch (error) {
+            throw new ConfigurationError(
+              'invalid refreshTime representation, check node library "ms" for the valid representation'
+            );
+          }
+          if (isNaN(interval)) {
+            throw new ConfigurationError(
+              'invalid refreshTime representation, check node library "ms" for the valid representation'
+            );
+          }
+          if (interval < 0) {
+            throw new ConfigurationError(
+              'invalid refreshTime representation, refreshTime can not be negitive'
+            );
+          }
+        }
+      });
+    }
     await next();
   }
 }
--- packages/build/test/schema-parser/middleware/checkCache.spec.ts
+++ packages/build/test/schema-parser/middleware/checkCache.spec.ts
@@ -1,46 +1,185 @@
+import { RawAPISchema } from '@vulcan-sql/build';
 import { CheckCache } from '@vulcan-sql/build/schema-parser/middleware/checkCache';
-import { RawAPISchema } from '../../../src';
-
 
 let middleware: CheckCache;
 const next = jest.fn();
 
 beforeEach(() => {
-    middleware = new CheckCache();
-    next.mockClear();
+  middleware = new CheckCache();
+  next.mockClear();
+});
+it('should call next() when there are no caches or cache metadata', async () => {
+  const schemas: RawAPISchema = {
+    sourceName: 'test',
+    metadata: {},
+  };
+  await middleware.handle(schemas, next);
+  expect(next).toHaveBeenCalled();
 });
 
-it('should not throw error if cache is not defined', async () => {
-    const schema: RawAPISchema = { sourceName: 'test' };
-    await middleware.handle(schema, next)
-    expect(next).toHaveBeenCalledTimes(1);
-  });
-
-it('should throw error if cache is defined but not used in SQL file', async () => {
-    const schema: RawAPISchema = { sourceName: 'test', cache: [{ cacheTableName: 'test', sql: 'select * from test' }] };
-    await expect(middleware.handle(schema,next)).rejects.toThrow('your SQL will use the cache feature, not YAML defined.');
-    expect(next).not.toHaveBeenCalled();
+it('should throw an error when caches is used but not been defined in schema.', async () => {
+  const schemas: RawAPISchema = {
+    sourceName: 'test',
+    metadata: {
+      'cache.vulcan.com': {
+        isUsedTag: true,
+      },
+    },
+  };
+  await expect(middleware.handle(schemas, next)).rejects.toThrow(
+    "Cache feature was used in SQL file, but didn't find the cache configurations in YAML file."
+  );
+  expect(next).not.toHaveBeenCalled();
 });
 
-it('should throw error if both refreshTime and refreshExpression are set', async () => {
-    const schema: RawAPISchema = { 
-        sourceName: 'test', 
-        cache: [
-        { cacheTableName: 'test1', sql: 'select * from test1', refreshTime: { every: '5m' }, refreshExpression: { expression: 'test' } },
-        { cacheTableName: 'test2', sql: 'select * from test2', refreshTime: { every: '5m' } }
-        ],
-        metadata: { 'cache.vulcan.com': { isUsedTag: true } }
-    };
-    await expect(middleware.handle(schema,next)).rejects.toThrow('can not configure refreshTime and refreshExpression at the same time, please pick one');
-    expect(next).not.toHaveBeenCalled();
+it('should throw an error when both refreshTime and refreshExpression are defined.', async () => {
+  const schemas: RawAPISchema = {
+    sourceName: 'test',
+    metadata: {
+      'cache.vulcan.com': {
+        isUsedTag: true,
+      },
+    },
+    cache: [
+      {
+        cacheTableName: 'test_cache',
+        sql: 'SELECT * FROM test_table',
+        refreshTime: { every: '5m' },
+        refreshExpression: { expression: '*/5,*,*,*,*', every: '5m' },
+      },
+    ],
+  };
+  await expect(middleware.handle(schemas, next)).rejects.toThrow(
+    'can not configure refreshTime and refreshExpression at the same time, please pick one'
+  );
+  expect(next).not.toHaveBeenCalled();
 });
 
-it('should call next if cache is defined and used in SQL file', async () => {
-    const schema: RawAPISchema = { 
-        sourceName: 'test', 
-        cache: [{ cacheTableName: 'test', sql: 'select * from test' }],
-        metadata: { 'cache.vulcan.com': { isUsedTag: true } }
-    };
-    await middleware.handle(schema, next);
-    expect(next).toHaveBeenCalledTimes(1);
+// refreshTime expression test cases
+it('should throw an error when an invalid refreshTime representation is used', async () => {
+  const schemas: RawAPISchema = {
+    sourceName: 'test',
+    metadata: {
+      'cache.vulcan.com': {
+        isUsedTag: true,
+      },
+    },
+    cache: [
+      {
+        cacheTableName: 'test_cache',
+        sql: 'SELECT * FROM test_table',
+        refreshTime: { every: 'invalidString' },
+      },
+    ],
+  };
+  await expect(middleware.handle(schemas, next)).rejects.toThrow(
+    'invalid refreshTime representation, check node library "ms" for the valid representation'
+  );
+  expect(next).not.toHaveBeenCalled();
+});
+
+it('should throw an error when a negative refreshTime interval is used', async () => {
+  const schemas: RawAPISchema = {
+    sourceName: 'test',
+    metadata: {
+      'cache.vulcan.com': {
+        isUsedTag: true,
+      },
+    },
+    cache: [
+      {
+        cacheTableName: 'test_cache',
+        sql: 'SELECT * FROM test_table',
+        refreshTime: { every: '-5m' },
+      },
+    ],
+  };
+  await expect(middleware.handle(schemas, next)).rejects.toThrow(
+    'invalid refreshTime representation, refreshTime can not be negitive'
+  );
+  expect(next).not.toHaveBeenCalled();
+});
+
+it('should call next function when schemas have cache with valid refreshTime representation', async () => {
+  const schemas: RawAPISchema = {
+    sourceName: 'test',
+    metadata: {
+      'cache.vulcan.com': {
+        isUsedTag: true,
+      },
+    },
+    cache: [
+      {
+        cacheTableName: 'test_cache',
+        sql: 'SELECT * FROM test_table',
+        refreshTime: { every: '5m' },
+      },
+    ],
+  };
+  await middleware.handle(schemas, next);
+  expect(next).toHaveBeenCalled();
+});
+
+// refreshExpression time expression test cases
+it('should throw an error when an invalid refreshExpression representation is used', async () => {
+  const schemas: RawAPISchema = {
+    sourceName: 'test',
+    metadata: {
+      'cache.vulcan.com': {
+        isUsedTag: true,
+      },
+    },
+    cache: [
+      {
+        cacheTableName: 'test_cache',
+        sql: 'SELECT * FROM test_table',
+        refreshExpression: { every: 'invalidString' },
+      },
+    ],
+  };
+  await expect(middleware.handle(schemas, next)).rejects.toThrow(
+    'invalid refreshTime representation, check node library "ms" for the valid representation'
+  );
+  expect(next).not.toHaveBeenCalled();
+});
+it('should throw an error when a negative refreshExpression interval is used', async () => {
+  const schemas: RawAPISchema = {
+    sourceName: 'test',
+    metadata: {
+      'cache.vulcan.com': {
+        isUsedTag: true,
+      },
+    },
+    cache: [
+      {
+        cacheTableName: 'test_cache',
+        sql: 'SELECT * FROM test_table',
+        refreshExpression: { every: '-5m' },
+      },
+    ],
+  };
+  await expect(middleware.handle(schemas, next)).rejects.toThrow(
+    'invalid refreshTime representation, refreshTime can not be negitive'
+  );
+  expect(next).not.toHaveBeenCalled();
+});
+
+it('should call next function when schemas have cache with valid refreshExpression representation', async () => {
+  const schemas: RawAPISchema = {
+    sourceName: 'test',
+    metadata: {
+      'cache.vulcan.com': {
+        isUsedTag: true,
+      },
+    },
+    cache: [
+      {
+        cacheTableName: 'test_cache',
+        sql: 'SELECT * FROM test_table',
+        refreshExpression: { every: '5m' },
+      },
+    ],
+  };
+  await middleware.handle(schemas, next);
+  expect(next).toHaveBeenCalled();
 });
