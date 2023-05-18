7046e6945d828845c53314ecd8ecf3dee7ffb48e
william chang(張仲威) <wwwy3y3@gmail.com>
2023-05-19 01:49:32 +0800

    suppress warning from aws-sdk

--- packages/extension-driver-snowflake/src/index.ts
+++ packages/extension-driver-snowflake/src/index.ts
@@ -1,3 +1,7 @@
+// suppress the warning message from aws-sdk: Please migrate your code to use AWS SDK for JavaScript (v3).
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('aws-sdk/lib/maintenance_mode_message').suppress = true;
+
 export * from './lib/snowflakeDataSource';
 import { SnowflakeDataSource } from './lib/snowflakeDataSource';
 export default [SnowflakeDataSource];
