189bd997ff333fe4f789ff69f2d5514385223ade
william chang(張仲威) <wwwy3y3@gmail.com>
2023-05-24 22:00:26 +0800

    fix(driver-snowflake): change the way to suppress the aws-sdk warning. Using require seems to cause module not found issue

--- packages/extension-driver-snowflake/src/index.ts
+++ packages/extension-driver-snowflake/src/index.ts
@@ -1,6 +1,7 @@
 // suppress the warning message from aws-sdk: Please migrate your code to use AWS SDK for JavaScript (v3).
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('aws-sdk/lib/maintenance_mode_message').suppress = true;
+// since in aws-sdk, they only check if AWS_SDK_JS_SUPPRESS_MAINTENANCE_MODE_MESSAGE exists, but not the value
+// setting it to a string value of 1 should work
+process.env['AWS_SDK_JS_SUPPRESS_MAINTENANCE_MODE_MESSAGE'] = '1';
 
 export * from './lib/snowflakeDataSource';
 import { SnowflakeDataSource } from './lib/snowflakeDataSource';
