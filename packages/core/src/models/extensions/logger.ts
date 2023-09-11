f8c1f8a4be2721762a58e393f288c4958fc45288
onlyjackfrost <onlyjackfrost@gmail.com>
2023-09-11 10:27:52 +0800

    rm console

--- packages/core/src/models/extensions/logger.ts
+++ packages/core/src/models/extensions/logger.ts
@@ -31,7 +31,6 @@ export abstract class BaseActivityLogger<ActivityLoggerTypeOption>
     const option = this.getConfig()['options'][
       this.getExtensionId()!
     ] as ActivityLoggerTypeOption;
-    console.log('option', option);
     return option;
   }
 }
