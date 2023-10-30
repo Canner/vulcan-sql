f0d7b1b72f4731b8de3ed09cf37cc8b0e22bc895
Ryosuke Lin Yamamoto <ryosuke.yamamoto@estie.co.jp>
2023-10-30 15:13:41 +0900

    allow return arbitrary value from filter extension

--- packages/core/src/lib/functional-extensions/filter.ts
+++ packages/core/src/lib/functional-extensions/filter.ts
@@ -16,7 +16,7 @@ export interface FunctionalFilterOptions {
 
 export type FunctionalFilter = (
   options: FunctionalFilterOptions
-) => Promise<string>;
+) => Promise<any>;
 
 export const createFilterExtension = (
   name: string,
