e9fd25155e7304fa395464e65d6727b38794eed9
andreashimin <andreashimin@gmail.com>
2024-01-15 18:56:43 +0800

    feat(admin-ui): add & adjust images foundation, refine less utilities

--- packages/admin-ui/components/LogoBar.tsx
+++ packages/admin-ui/components/LogoBar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function LogoBar() {
   return <Space>
-    <Image src="/logo.svg" alt="Logo" width={40} height={40} />
+    <Image src="/images/logo.svg" alt="Logo" width={40} height={40} />
     <span className="gray-8">VulcanSQL</span>
   </Space>;
 }
