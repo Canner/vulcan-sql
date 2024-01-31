383cccea5f79728ceea68aeda393b84bafc84fc2
hanshino <c0952199517@gmail.com>
2024-01-31 16:53:00 +0800

    Add types for micro-cors and context

--- packages/admin-ui/pages/api/graphql.ts
+++ packages/admin-ui/pages/api/graphql.ts
@@ -1,20 +1,25 @@
-import * as microCors from 'micro-cors';
+import microCors from 'micro-cors';
 import { NextApiRequest, NextApiResponse, PageConfig } from 'next';
 import { ApolloServer } from 'apollo-server-micro';
 import { typeDefs, resolvers } from '@vulcan-sql/admin-ui/apollo/server';
+import { IContext } from '@vulcan-sql/admin-ui/apollo/server/types';
 
 const cors = microCors();
 
 export const config: PageConfig = {
   api: {
-    bodyParser: false
-  }
-}
+    bodyParser: false,
+  },
+};
 
 const apolloServer: ApolloServer = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: process.env.NODE_ENV !== 'production',
+  context: (): IContext => ({
+    // Place your context like repository, service layer here
+    // modelRepository: new ModelRepository(),
+  }),
 });
 
 const startServer = apolloServer.start();
@@ -25,7 +30,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     path: '/api/graphql',
   })(req, res);
 };
-
 
 export default cors((req: NextApiRequest, res: NextApiResponse) =>
   req.method === 'OPTIONS' ? res.status(200).end() : handler(req, res)
