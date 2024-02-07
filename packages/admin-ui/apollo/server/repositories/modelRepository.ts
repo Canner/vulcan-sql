c788add9707e21ac32e30579ecfbe1b72db3b519
onlyjackfrost <onlyjackfrost@gmail.com>
2024-02-07 14:16:51 +0800

    fix lint

--- packages/admin-ui/apollo/server/repositories/modelRepository.ts
+++ packages/admin-ui/apollo/server/repositories/modelRepository.ts
@@ -1,5 +1,5 @@
 import { Knex } from 'knex';
-import { BaseRepository, IQueryOptions } from './baseRepository';
+import { BaseRepository } from './baseRepository';
 
 export interface Model {
   id: number; // ID
