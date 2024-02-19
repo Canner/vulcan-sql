05525daf7f764475b649f5ee582117583a09a45c
onlyjackfrost <onlyjackfrost@gmail.com>
2024-02-19 15:17:28 +0800

    fix bq connector

--- packages/admin-ui/apollo/server/connectors/bqConnector.ts
+++ packages/admin-ui/apollo/server/connectors/bqConnector.ts
@@ -5,13 +5,15 @@ import { BigQuery, BigQueryOptions } from '@google-cloud/bigquery';
 export class BQConnector implements IConnector {
   private bq: BigQuery;
 
+  // Not storing the bq client instance because we rarely need to use it
   constructor(bqOptions: BigQueryOptions) {
     this.bq = new BigQuery(bqOptions);
   }
 
   public async connect() {
     try {
-      const res = await this.bq.query('SELECT 1;');
+      await this.bq.query('SELECT 1;');
+      return true;
     } catch (err) {
       return false;
     }
