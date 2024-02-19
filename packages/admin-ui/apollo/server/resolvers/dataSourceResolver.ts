dffb98d0b6d134c337e71003f4ca31f42a5028c5
onlyjackfrost <onlyjackfrost@gmail.com>
2024-02-19 15:19:40 +0800

    encrypt credentials before storing

--- packages/admin-ui/apollo/server/resolvers/dataSourceResolver.ts
+++ packages/admin-ui/apollo/server/resolvers/dataSourceResolver.ts
@@ -1,13 +1,12 @@
 import { BigQueryOptions } from '@google-cloud/bigquery';
 import { BQConnector } from '../connectors/bqConnector';
-import {
-  DataSource,
-  DataSourceName,
-  IContext,
-  UsableDataSource,
-} from '../types';
+import { DataSource, DataSourceName, IContext } from '../types';
+import crypto from 'crypto';
 import * as fs from 'fs';
+import path from 'path';
+import { getLogger, Encryptor } from '@vulcan-sql/admin-ui/apollo/server/utils';
 
+const logger = getLogger('DataSourceResolver');
 export class DataSourceResolver {
   constructor() {
     this.saveDataSource = this.saveDataSource.bind(this);
@@ -23,19 +22,24 @@ export class DataSourceResolver {
     const { type, properties } = args.data;
     if (type === DataSourceName.BIG_QUERY) {
       await this.saveBigQueryDataSource(properties, ctx);
+      return args.data;
     }
   }
 
   private async saveBigQueryDataSource(properties: any, ctx: IContext) {
     const { location, projectId, credentials } = properties;
+    const { config } = ctx;
     let filePath = '';
     try {
       // check DataSource is valid and can connect to it
-      filePath = await this.writeCredentialsFile(credentials);
+      filePath = await this.writeCredentialsFile(
+        credentials,
+        config.persistCredentialDir
+      );
       const connectionOption: BigQueryOptions = {
-        location: location,
-        projectId: projectId,
-        keyFile: filePath,
+        location,
+        projectId,
+        keyFilename: filePath,
       };
       const connector = new BQConnector(connectionOption);
       const connected = await connector.connect();
@@ -43,18 +47,18 @@ export class DataSourceResolver {
         throw new Error('Cannot connect to DataSource');
       }
       // save DataSource to database
-      const encryptedCredentials = await this.encryptCredentials(credentials);
+      const encryptor = new Encryptor(config);
+      const encryptedCredentials = await encryptor.encrypt(credentials);
 
       const project = await ctx.projectRepository.createOne({
         displayName: 'tbd',
         schema: 'tbd',
         catalog: 'tbd',
         type: 'BIG_QUERY',
+        projectId,
         credentials: encryptedCredentials,
       });
       return project;
-    } catch (e) {
-      throw e;
     } finally {
       // remove the file
       if (filePath) {
@@ -63,17 +67,26 @@ export class DataSourceResolver {
     }
   }
 
-  private async encryptCredentials(credentials: string) {
-    // encrypt credentials
-    return credentials;
-  }
+  private async writeCredentialsFile(
+    credentials: JSON,
+    persist_credential_dir: string
+  ) {
+    // file name will be the hash of the credentials, file path is current working directory
+    // convert credentials from base64 to string and replace all the matched "\n" with "\\n",  there are many \n in the "private_key" property
+    const credentialString = JSON.stringify(credentials);
+    const fileName = crypto
+      .createHash('md5')
+      .update(credentialString)
+      .digest('hex');
 
-  private async decryptCredentials(credentials: string) {
-    return credentials;
-  }
-
-  private async writeCredentialsFile(credentials: string) {
-    // write credentials to file
-    return 'file_path';
+    const filePath = path.join(persist_credential_dir, `${fileName}.json`);
+    // check if file exists
+    if (fs.existsSync(filePath)) {
+      logger.debug(`File ${filePath} already exists`);
+      return filePath;
+    }
+    logger.debug(`Writing credentials to file ${filePath}`);
+    fs.writeFileSync(filePath, credentialString);
+    return filePath;
   }
 }
