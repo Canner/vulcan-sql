9d152e08d14a43dd991bb736d0e23fec748353c3
Freda Lai <fredalai00@gmail.com>
2024-02-21 10:11:08 +0800

    feat(admin-ui): allow renaming when the onBlur event is trigger

--- packages/admin-ui/components/sidebar/exploration/ExplorationTitleInput.tsx
+++ packages/admin-ui/components/sidebar/exploration/ExplorationTitleInput.tsx
@@ -24,6 +24,7 @@ export default function ExplorationTitleInput(props: {
         onSetTitle(e.target.value)
       }
       onPressEnter={(e) => onRename(title)}
+      onBlur={(e) => onRename(title)}
     />
   );
 }
