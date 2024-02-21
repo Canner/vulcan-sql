14809a431e000df4da87df2fd890c389d72f4606
onlyjackfrost <onlyjackfrost@gmail.com>
2024-02-21 18:51:15 +0800

    example of reusing cached data accross apis

--- labs/playground1/sqls/artist/artist.sql
+++ labs/playground1/sqls/artist/artist.sql
@@ -1,8 +1,5 @@
+{% cache %}
 select 
 *
-from "artists"
-where
-ConstituentID = {{ context.params.id }}
-{% if contex.params.begin_date %}
-    and BeginDate = {{ context.params.begin_date }}
-{% endif %}
+from "artist_cache"
+{% endcache %}
---
+++ labs/playground1/sqls/artist/artist_cache.sql
@@ -0,0 +1,1 @@
+select * from vulcan.artist_cache
