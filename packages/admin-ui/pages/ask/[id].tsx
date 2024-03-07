779329afe9e7b8d484f20c2a936e6d80aecf9092
Freda Lai <fredalai00@gmail.com>
2024-03-07 16:28:00 +0800

    feat(admin-ui): adjust style and auto-scroll to the page bottom for answer block

--- packages/admin-ui/pages/ask/[id].tsx
+++ packages/admin-ui/pages/ask/[id].tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import { Divider } from 'antd';
@@ -31,8 +32,27 @@ const AnswerResultsBlock = styled.div`
 `;
 
 export default function AnswerBlock() {
+  const divRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
   const saveAsViewModal = useModalAction();
+
+  // TODO: implement scroll when has new answer result
+  useEffect(() => {
+    if (divRef.current) {
+      const contentLayout = divRef.current.parentElement;
+      const lastChild = divRef.current.lastElementChild as HTMLElement;
+      const lastChildDivider = lastChild.firstElementChild as HTMLElement;
+      if (
+        contentLayout.clientHeight <
+        lastChild.offsetTop + lastChild.clientHeight
+      ) {
+        contentLayout.scrollTo({
+          top: lastChildDivider.offsetTop,
+          behavior: 'smooth',
+        });
+      }
+    }
+  }, [divRef]);
 
   // TODO: call API to get real exploration list data
   const data = [];
@@ -72,7 +92,7 @@ export default function AnswerBlock() {
 
   return (
     <SiderLayout loading={false} sidebar={{ data, onSelect }}>
-      <AnswerResultsBlock className="my-12">
+      <AnswerResultsBlock className="mt-12 mb-15" ref={divRef}>
         {answerResults.map((answerResult, index) => (
           <div key={`${answerResult.query}-${index}`}>
             {index > 0 && <Divider />}
